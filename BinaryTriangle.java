6ee625ad1c08bb8e11b33af9a44c59cecf88c882
poojadhumal12 <poojadhumal12092002@gmail.com>
2026-03-14 14:21:56 +0530

    Implement Binary Triangle pattern in Java

--- BinaryTriangle.java
+++ BinaryTriangle.java
@@ -1,1 +1,23 @@
+public class BinaryTriangle {
+    public static void main(String[] args) {
 
+        int rows = 6;
+
+        for (int i = 1; i <= rows; i++) {
+
+            int num = i % 2;
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+
+                if (num == 1) {
+                    num = 0;
+                } else {
+                    num = 1;
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
